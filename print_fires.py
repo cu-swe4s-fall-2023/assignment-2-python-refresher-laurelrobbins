39dbd78df49ae15e33241c0848f0a4794bea6284
Laurel Robbins <laurel.robbins@colorado.edu>
2023-09-13 10:30:12 -0600

    edited my_utils to add data to list for specified countries only

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,16 @@
-country='United States of America'
-county_column = 1
-fires_column = 4
+import my_utils
+#query_column = country_column
+#query_value = country
+#result_column = fires_column 
+
+#if country_column = country, add it to array. This is how we pick specific fields from the data set. 
+#country 
+query_value = 'United States of America'
+query_column = 0
+result_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
+
+fires = my_utils.get_column(file_name, query_column, query_value, result_column)
+
 print(fires)
+
