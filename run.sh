3a1c3117e957f8262027294b6e8140532fd0a41c
laurelrobbins <143537495+laurelrobbins@users.noreply.github.com>
2023-09-28 12:08:36 -0600

    Delete run.sh due to duplicate

--- run.sh
+++
@@ -1,6 +0,0 @@
-python print_fires.py --file_name 'Agrofood_co2_emission.csv' --query_column 0 --query_value 'United States of America' --result_column 3
-
-python print_fires.py --file_name 'nonexistent_file.csv' --query_column 0 --query_value 'United States of America' --result_column 3
-
-python print_fires.py --file_name 'Agrofood_co2_emission.csv' --query_column 0 --query_value 'United States of America' --result_column 'unexpected_string'
-
